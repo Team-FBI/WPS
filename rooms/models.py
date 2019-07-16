12c235faf35f78b7ff3480dc239e9de26fbc13f7
DiceMono <qrdfg@naver.com>
2019-07-16 17:00:50 +0900

    update reservations in models.py

--- rooms/models.py
+++ rooms/models.py
@@ -4,8 +4,6 @@ from django.utils.text import slugify
 from locations.models import State
 # Create your models here.
 
-
-
 def n_tuple(n, first=[], last=[]):
     return tuple(first + [(i, i) for i in range(1, n)] + last)
 
@@ -14,12 +12,12 @@ NO_OF_ROOMS = n_tuple(10)
 MIN_STAY = n_tuple(90)
 MAX_STAY = n_tuple(60, first=[(0, "Unlimited")])
 NO_OF_BEDS = n_tuple(20, first=[(0, "-")])
-PLACE_RATING = n_tuple(6, first=[(0, "Not rated")])
+room_RATING = n_tuple(6, first=[(0, "Not rated")])
 ORDER = n_tuple(20, first=[(0, "-")])
 MAX_GUEST = n_tuple(20, first=[(0, "-")])
 
 
-PLACE_TYPES = [
+room_TYPES = [
     (1, "Apartment"),
     (2, "House"),
     (3, "Garden House"),
@@ -29,7 +27,7 @@ PLACE_TYPES = [
     (50, "Office"),
 ]
 
-SPACE_TYPES = [(1, "Entire Place"), (2, "Private Room"), (3, "Shared Room")]
+SPACE_TYPES = [(1, "Entire room"), (2, "Private Room"), (3, "Shared Room")]
 
 BATHROOM_TYPES = [(1, "Private"), (2, "Shared")]
 
@@ -38,10 +36,10 @@ CANCELATION_RULES = [(1, "Flexible"), (2, "Semi-flexible"), (3, "Strict")]
 UNAVAIL_REASON = [(1, "Unavailable"), (2, "Requested"), (3, "Booked")]
 
 PHOTO_TYPES = [
-    (1, "Inside of the place"),
-    (2, "View of the place"),
-    (3, "External appearance of the place"),
-    (4, "Around the place"),
+    (1, "Inside of the room"),
+    (2, "View of the room"),
+    (3, "External appearance of the room"),
+    (4, "Around the room"),
     (4, "Other"),
 ]
 
@@ -89,7 +87,7 @@ class Room(models.Model):
     image = models.ImageField(upload_to=f"rooms/%Y/%m/%d/", blank=True, null=True)
     price = models.PositiveIntegerField(blank=True, null=True)
     capacity = models.SmallIntegerField(choices=NO_OF_BEDS, default=6)
-    room_type = models.SmallIntegerField(choices=PLACE_TYPES, default=1)
+    room_type = models.SmallIntegerField(choices=room_TYPES, default=1)
     space = models.SmallIntegerField(choices=SPACE_TYPES, default=1)
     bedroom = models.SmallIntegerField(choices=NO_OF_ROOMS, default=1)
     bed_type = models.SmallIntegerField(choices=BATHROOM_TYPES, default=1)
@@ -98,16 +96,19 @@ class Room(models.Model):
     min_stay = models.SmallIntegerField(choices=MIN_STAY, default=1)
     max_stay = models.SmallIntegerField(choices=MAX_STAY, default=0)
     description = models.TextField(blank=True, null=True)
-    accuracy_rating = models.SmallIntegerField(choices=PLACE_RATING, default=0)
-    location_rating = models.SmallIntegerField(choices=PLACE_RATING, default=0)
-    communication_rating = models.SmallIntegerField(choices=PLACE_RATING, default=0)
-    checkin_rating = models.SmallIntegerField(choices=PLACE_RATING, default=0)
-    clean_rating = models.SmallIntegerField(choices=PLACE_RATING, default=0)
-    value_rating = models.SmallIntegerField(choices=PLACE_RATING, default=0)
+    accuracy_rating = models.SmallIntegerField(default=0)
+    location_rating = models.SmallIntegerField(default=0)
+    communication_rating = models.SmallIntegerField(default=0)
+    checkin_rating = models.SmallIntegerField(default=0)
+    clean_rating = models.SmallIntegerField(default=0)
+    value_rating = models.SmallIntegerField(default=0)
+    total_rating = models.SmallIntegerField(default=0)
 
     active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
 
     def save(self):
         self.slug = slugify(self.title, allow_unicode=True)
@@ -116,8 +117,45 @@ class Room(models.Model):
     class Meta:
         ordering = ["-created_at", "-updated_at"]
         # get_latest_by = "timestamp"
-        # verbose_name = 'Place'
+        # verbose_name = 'room'
 
     def __str__(self):
         return f"{self.slug} / {self.host}"
+
+
+class ReservedDates(models.Model):
+
+    start_date = models.DateField()
+    end_date = models.DateField()
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='reserveds')
+
+
+class Booking(models.Model):
+
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, )
+    reservation = models.ForeignKey(ReservedDates, on_delete=models.CASCADE, related_name='booking')
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='booking')
+    price = models.PositiveIntegerField(default=0)
+    number_guest = models.PositiveIntegerField(default=0)
+    nights = models.PositiveIntegerField(default=0)
+
+
+class RoomReview(models.Model):
+    """장소에 대한 유저의 리뷰"""
+    writer = models.ForeignKey(get_user_model(), on_delete=models.DO_NOTHING, related_name='room_review')
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='review')
+    booking = models.ForeignKey(Booking, on_delete=models.CASCADE, related_name='room_review')
+    text = models.TextField(blank=True)
+
+    active = models.BooleanField(default=True)
+
+    rating_1 = models.SmallIntegerField()
+    rating_2 = models.SmallIntegerField()
+    total_rating = models.SmallIntegerField(blank=True)
+
+    def save(self, *args, **kwargs):
+        self.total_rating = round(((self.rating_1 + self.rating_2) / 2), 2)
+        super(RoomReview, self).save()
+        self.room.room_rating()
+
 
