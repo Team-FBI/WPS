f37fe8eadd7313ed3ba9a35b02fcb559cf531c84
junehan <codenamenadja@gmail.com>
2019-07-21 13:50:26 +0900

    room filterings

--- rooms/views.py
+++ rooms/views.py
@@ -3,7 +3,8 @@ from rest_framework.viewsets import generics
 from rest_framework.response import Response
 from rest_framework import exceptions
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from .models import Room, Reservation
+from django_filters.rest_framework import DjangoFilterBackend
+from rooms.models import Room, Reservation
 from rooms.serializers import (
     RoomListSerializer,
     RoomCreateSerializer,
@@ -12,30 +13,22 @@ from rooms.serializers import (
 )
 from config.utils import response_error_handler
 
+class PriceFilterBackend(filters.BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        min_price = request.query_params.get("min_price", 0)
+        max_price = request.query_params.get("max_price", None)
+        condition_min = Q(price__gte=min_price)
+        if not max_price:
+            return queryset.filter(condition_min)
+        condition_max = Q(price__lte=max_price)
+        return queryset.filter(condition_min & condition_max)
 
-def filter_backend(queryset, query_params):
-    order = query_params.get("order", "created_at")
-    perpage = int(query_params.get("perpage", 10))
-    page = int(query_params.get("page", 1))
-    queryset = queryset.order_by(order)
 
-    min_price = query_params.get("min_price", None)
-    max_price = query_params.get("max_price", None)
-    capacity = query_params.get("capacity", None)
-
-    try:
-        if min_price:
-            queryset = queryset.filter(price__gte=min_price)
-        if max_price:
-            queryset = queryset.filter(price__lte=max_price)
-        if capacity:
-            queryset = queryset.filter(capacity__gte=capacity)
-    except Exception:
-        raise AttributeError("filter attribute error", "querystrng wasnt good")
-    paged = queryset[(perpage * page - perpage) : (perpage * page)]
-    if len(paged):
-        return paged
-    return queryset
+class StandardResultSetPagination(PageNumberPagination):
+    page_size = 12
+    max_page_size = 100
+    page_query_param = "page"
+    page_size_query_param = "page_size"
 
 
 class RoomListView(generics.ListAPIView):
@@ -44,17 +37,17 @@ class RoomListView(generics.ListAPIView):
     Arguments:
         viewsets {[ListAPIView]} -- [GET handler]
     QuerystringOptions:
-        #ordering
-        order -- [default created_at, order by numeric values with "-" mark]
-        perpage -- [default 10, data amount in page]
-        page -- [default 0, page of data-perpage]
+    #ordering-required
+        ordering -- [default update_at, price, updated_at, created_at, total_rating]
+        page_size -- [default 12, data amount in page]
+        page -- [default 1, page of data-perpage]
 
-        #filterings
-        state|country -- [default All, filter by state or country]
-        host -- [default All, filter by host username]
+    #filterings-required
+        search -- [could come state or country or part of host email]
+
+    #filterings-Non_required
         min_price -- [default All, filter price greater than input]
         max_price -- [default All, filter price lower than input]
-        capacity -- [default All, filter capacity greater than input]
     Raises:
         AttributeError: [GET-HTTP_400_BAD_REQUEST]
     Returns:
@@ -74,23 +67,26 @@ class RoomListView(generics.ListAPIView):
     """
 
     serializer_class = RoomListSerializer
-
-    def get_queryset(self):
-        queryset = Room.objects.all()
-        state = self.request.query_params.get("state", None)
-        country = self.request.query_params.get("country", None)
-        host = self.request.query_params.get("host", None)
-        if state:
-            queryset = queryset.filter(state__name=state)
-        elif country:
-            queryset = queryset.filter(state__country__name=country)
-        if host:
-            queryset = queryset.filter(host__username=host)
-        queryset = filter_backend(queryset, self.request.query_params)
-        return queryset
+    pagination_class = StandardResultSetPagination
+    queryset = Room.objects.all()
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter, PriceFilterBackend]
+    search_fields = ["=state__name", "=state__country__name", "^host__username"]
+    ordering_fields = ["price", "created_at", "updated_at", "total_rating"]
+    ordering = ["updated_at"]
 
     @response_error_handler
     def get(self, request, *args, **kwargs):
+        # print(request.query_params)
+        query_order = request.query_params.get("ordering")
+        query_search = request.query_params.get("search")
+        page = request.query_params.get("page")
+        page_size = request.query_params.get("page_size")
+        if not all([query_search, query_order, page, page_size]):
+            raise AttributeError("all Query string not specified", "?ordering=price")
+        if query_order not in self.ordering_fields:
+            raise AttributeError(
+                "query of ordering not matched", f"specify in {self.ordering_fields}"
+            )
         return super().list(request, *args, **kwargs)
 
 
@@ -201,7 +197,6 @@ class RoomDetailView(generics.RetrieveAPIView):
     def get_queryset(self):
         room_id = self.kwargs.get("pk", None)
         queryset = Room.objects.filter(id=room_id)
-        print(queryset[0])
         if not (queryset[0] and room_id):
             raise ValueError("Room id Not Found", "check room id")
         return queryset
@@ -263,12 +258,16 @@ class CreateRoomReview(generics.CreateAPIView):
         room = reservation.room_for
         if not RoomReview.objects.filter(reservation_for=reservation).exists():
             serializer = RoomReviewCreateSerializer(
-                data=request.data, user=user, place_for=room, reservation_for=reservation)
+                data=request.data,
+                user=user,
+                place_for=room,
+                reservation_for=reservation,
+            )
             if serializer.is_valid():
                 self.perform_create(serializer)
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return Response({"error":"이미 후기를 작성하였습니다."})
+            return Response({"error": "이미 후기를 작성하였습니다."})
 
 
 class RoomReviewListView(generics.ListAPIView):
@@ -282,5 +281,6 @@ class RoomReviewDetailView(generics.RetrieveUpdateDestroyAPIView):
     Retrieve, Update, and Delete comment endpoint
     Allowed request method: Get, Post, Delete
     """
+
     queryset = RoomReview.objects.all()
     serializer_class = RoomReviewDetailSerializer
