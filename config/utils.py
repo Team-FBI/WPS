614e0c91d6eccdebb401cddbdd6ac3b6de11d7ad
junehan <codenamenadja@gmail.com>
2019-07-24 06:28:52 +0900

    error handler function corrections

--- config/utils.py
+++ config/utils.py
@@ -1,4 +1,4 @@
-from typing import Callable, List, Dict
+from typing import Callable, List, Dict, Union
 from functools import wraps
 from rest_framework import status
 from rest_framework.response import Response
@@ -7,40 +7,32 @@ from rest_framework.exceptions import ErrorDetail, ValidationError
 
 def response_error_handler(func: Callable) -> Callable:
     @wraps(func)
-    def _wrapper(*args: List, **kwargs: Dict) -> Callable:
+    def _wrapper(*args: List, **kwargs: Dict) -> Union[Callable, Response]:
         try:
             return func(*args, **kwargs)
-        except Exception as e:
-            err = BaseException("Base Error", "sorry, Unknown Error.")
-            res_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        except BaseException as e:
+            e: BaseException
+            err: BaseException
+            res_code: int
+            err = BaseException(
+                "Unknow Interner server error",
+                "We will log this and solve problem soon",
+            )
+            res_code = status.HTTP_423_LOCKED
             if isinstance(e, ConnectionRefusedError):
                 err = e
                 res_code = status.HTTP_405_METHOD_NOT_ALLOWED
-            if isinstance(e, PermissionError):
+            elif isinstance(e, PermissionError):
                 err = e
                 res_code = status.HTTP_401_UNAUTHORIZED
-            if isinstance(e, ValidationError):
-                err = ValueError(e.detail, "Re-type form data")
+            elif isinstance(e, ValidationError):
+                err = Exception(e.detail, "retype form data")
                 res_code = status.HTTP_400_BAD_REQUEST
-            if isinstance(e, AttributeError):
-                err = e
-                res_code = status.HTTP_400_BAD_REQUEST
-            if isinstance(e, ValueError):
+            elif isinstance(e, (AttributeError, ValueError)):
                 err = e
                 res_code = status.HTTP_400_BAD_REQUEST
             data = {"alert": err.args[0], "solution": err.args[-1]}
             return Response(data=data, status=res_code)
+
     return _wrapper
 
-
-def update(self, request, *args, **kwargs):
-    partial = kwargs.pop("partial", False)
-    instance = self.get_object()
-    serializer = self.get_serializer(instance, data=request.data, partial=partial)
-    serializer.is_valid(raise_exception=True)
-    self.perform_update(serializer)
-
-    if getattr(instance, "_prefetched_objects_cache", None):
-        instance._prefetched_objects_cache = {}
-
-    return Response(data=None, status=status.HTTP_201_NO_CONTENT)
