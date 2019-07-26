22c8816ee615db72625b028a443f5a9c8903a80f
DiceMono <qrdfg@naver.com>
2019-07-26 13:50:25 +0900

    add Message Model

--- chat/models.py
+++ chat/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from reservations.models import RoomReservation
 
-# Create your models here.
 
 class Message(models.Model):
-    pass
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    reservation = models.ForeignKey(RoomReservation, on_delete=models.CASCADE, related_name='messages')
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
