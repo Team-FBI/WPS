aca1a38513d03d9521436d1db6d9edce8184bb73
junehan <codenamenadja@gmail.com>
2019-07-29 21:30:00 +0900

    email reverts

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
 class User(AbstractUser):
-    email = models.EmailField(unique=True)
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to="user_image/profile/%Y/%m/%d/", blank=True)
 
