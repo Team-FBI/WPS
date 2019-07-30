dabc32d77a3f92988b31ea7db6542df904d0cf1f
DiceMono <qrdfg@naver.com>
2019-07-30 16:41:41 +0900

    update is_host field in Message Model

--- chat/models.py
+++ chat/models.py
@@ -5,6 +5,7 @@ from reservations.models import RoomReservation
 
 class Message(models.Model):
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    is_host = models.BooleanField(default=False)
     reservation = models.ForeignKey(RoomReservation, on_delete=models.CASCADE, related_name='messages')
     text = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
