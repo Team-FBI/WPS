f56554ee446abaa6c868650d9f4418193a524485
kimnamhyung <lutorydream@gmail.com>
2019-07-31 17:36:13 +0900

    fix error

--- trips/admin.py
+++ trips/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(State)
 admin.site.register(Provide)
 admin.site.register(TripProvide)
 admin.site.register(Trip)
