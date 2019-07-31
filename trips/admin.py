7f107e696791b8d9769ff0174931335810b3e728
kimnamhyung <lutorydream@gmail.com>
2019-07-31 17:02:45 +0900

    트립 추가

---
+++ trips/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+admin.site.register(State)
+admin.site.register(Provide)
+admin.site.register(TripProvide)
+admin.site.register(Trip)
+admin.site.register(TripSchedule)
+admin.site.register(TripCategory)
+admin.site.register(SubTripCategory)
+admin.site.register(Reservation)
+admin.site.register(TripReview)
