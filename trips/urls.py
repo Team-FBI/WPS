3f1cd31e7292e0c96df691cd53d50bb179d31479
kimnamhyung <lutorydream@gmail.com>
2019-07-31 18:15:18 +0900

    fixed trips.urls.py

--- trips/urls.py
+++ trips/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     path("trip-category/", TripCategoryList.as_view(), name=TripCategoryList.name),
     path("trip-category/<int:pk>/", TripCategoryDetail.as_view(), name=TripCategoryDetail.name),
-    path("/", TripList.as_view(), name=TripList.name),
+    path("", TripList.as_view(), name=TripList.name),
     path("<int:pk>/", TripDetail.as_view(), name=TripDetail.name),
     path("reservation/", TripReservationCreate.as_view(), name=TripReservationCreate.name),
     path("review/", TripReviewCreate.as_view(), name=TripReviewCreate.name),
