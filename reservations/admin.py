b8d714b95f3ad32e17fa7619a82e76ed887a4054
junehan <codenamenadja@gmail.com>
2019-08-02 20:46:20 +0900

    admin reservation setting

--- reservations/admin.py
+++ reservations/admin.py
@@ -2,4 +2,17 @@ from django.contrib import admin
 from reservations.models import RoomReservation
 # Register your models here.
 
-admin.site.register(RoomReservation)
+class ReservationOption(admin.ModelAdmin):
+    list_display = ["id", "room", "user", "start_date", "end_date"]
+    list_display_links = ("id", "room", "user")
+    readonly_fields = [
+        "accuracy_score",
+        "location_score",
+        "checkin_score",
+        "clean_score",
+        "communication_score",
+        "value_score",
+        "is_active",
+    ]
+
+admin.site.register(RoomReservation, ReservationOption)
